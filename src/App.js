f6efbfcdf53988486115d197a6362526d909c167
david <daviddecastro42@live.co.uk>
2021-02-20 23:43:51 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>hello word</h1>
+    </div>
+  );
+}
+
+export default App;
