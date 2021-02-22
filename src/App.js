1eca015cd246ed51f31b9426806fdc0087f6f492
david <daviddecastro42@live.co.uk>
2021-02-22 11:42:56 +0000

    building two component tours and tour to user props to connect with other component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  // const [loading, setLoading] = useEffect(true);
+  // const [tours, setTours] = useEffect([]);
+
   return (
-    <div className='App'>
-      <h1>hello word</h1>
-    </div>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
---
+++ src/Tour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tour() {
+  return (
+    <div>
+      <h2>hello tour</h2>
+    </div>
+  );
+}
+
+export default Tour;
